257708e5a941f1a23f6a36b7e9b29e9f030082ab
MonzonAxel <axel.monzon15@hotmail.com>
2023-05-19 01:04:48 -0300

    changed

--- Clase12-for/scripts.js
+++ Clase12-for/scripts.js
@@ -19,9 +19,11 @@ let numbers = [56,14,23,37,41,59]
 
 for(let i=0;i<numbers.length;i++){
 
-    console.log(`i vale ${i} y el valor de esa posición en el array es ${numbers[i]}`);
+    console.log(`${numbers[i]}`);
 
 }
 
-
+for( let names of numbers){
+    console.log(names);
+}
 
