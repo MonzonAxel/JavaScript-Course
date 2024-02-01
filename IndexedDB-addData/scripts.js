66fcf8a7c4f5a6b87326778061c1f8da1d520893
MonzonAxel <axel.monzon15@hotmail.com>
2024-02-01 10:57:54 -0300

    IndexedDB pure

---
+++ IndexedDB-addData/scripts.js
@@ -0,0 +1,41 @@
+//Especificación oficial: https://developer.mozilla.org/es/docs/IndexedDB-840092-dup
+
+const indexedDB = window.indexedDB
+const form = document.getElementById('form')
+
+if (indexedDB && form) {
+    let db
+    const request = indexedDB.open('tasksList', 1)
+
+    request.onsuccess = () => {
+        db = request.result
+        console.log('OPEN', db)
+    }
+
+    request.onupgradeneeded = (e) => {
+        db = e.target.result
+        console.log('Create', db)
+        const objectStore = db.createObjectStore('tasks', {
+            autoIncrement: true
+        })
+    }
+    
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const data = {
+            taskTitle: e.target.task.value,
+            taskPriority: e.target.priority.value
+        }
+        addData(data)
+    })
+
+    const addData = (data) => {
+        const transaction = db.transaction(['tasks'], 'readwrite')
+        const objectStore = transaction.objectStore('tasks')
+        const request = objectStore.add(data)
+    }
+    
+    request.onerror = (error) => {
+    console.log('Error', error)
+    }
+}
